5ce6d6c9d966a57154bad6a24769ed77a74bb548
Nataša Đurić <natasa.djuric@student.um.si>
2024-04-24 23:28:38 +0200

    Viewing of the profiles enabled

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,18 @@
-import { useContext } from "react";
-import { UserContext } from "../userContext";
-import { Link } from "react-router-dom";
+import {useContext} from "react";
+import {UserContext} from "../userContext";
+import {Link} from "react-router-dom";
 
 function Header(props) {
+    const userContext = useContext(UserContext);
+    const username = userContext.user ? userContext.user.username : "";
+
     return (
         <header className="mb-5">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
-                        <img src="https://i.imgur.com/5hqFNND.png" width="35" height="30" className="d-inline-block align-top" alt="Icon" />
+                        <img src="https://i.imgur.com/5hqFNND.png" width="35" height="30"
+                             className="d-inline-block align-top" alt="Icon"/>
                     </Link>
                     <div className="collapse navbar-collapse" id="navbarColor01">
                         <ul className="navbar-nav mr-auto">
@@ -20,7 +24,7 @@ function Header(props) {
                                                 <Link className="nav-link" to="/publish">Publish</Link>
                                             </li>
                                             <li className="nav-item ml-auto">
-                                                <Link className="nav-link" to="/profile">Profile</Link>
+                                                <Link className="nav-link" to={`/profile/${username}`}>Profile</Link>
                                             </li>
                                             <li className="nav-item">
                                                 <Link className="nav-link" to="/logout">Logout</Link>
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,26 +1,44 @@
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../userContext';
-import { Navigate } from 'react-router-dom';
+import {useContext, useEffect, useState} from 'react';
+import {UserContext} from '../userContext';
+import {Navigate, useParams} from 'react-router-dom';
 
-function Profile(){
-    const userContext = useContext(UserContext); 
+//TODO: Make the profile page show different information based on if the viewer is the owner of the profile or not
+
+function Profile() {
+    const userContext = useContext(UserContext);
+    const {username} = useParams();
     const [profile, setProfile] = useState({});
 
-    useEffect(function(){
-        const getProfile = async function(){
-            const res = await fetch("http://localhost:3001/users/profile", {credentials: "include"});
-            const data = await res.json();
-            setProfile(data);
+    useEffect(function () {
+        const getProfile = async () => {
+            try {
+                const res = await fetch(`http://localhost:3001/users/${username}`, {credentials: "include"});
+                if (!res.ok) {
+                    throw new Error('Failed to fetch profile data');
+                }
+                const data = await res.json();
+                console.log(data); // Log the fetched data
+                setProfile(data);
+            } catch (error) {
+                console.error(error); // Log any errors
+            }
         }
         getProfile();
-    }, []);
+    }, [username]);
+
 
     return (
         <>
-            {!userContext.user ? <Navigate replace to="/login" /> : ""}
-            <h1>User profile</h1>
-            <p>Username: {profile.username}</p>
-            <p>Email: {profile.email}</p>
+            {!userContext.user && <Navigate replace to="/login"/>}
+            <div className="container">
+                <div className="card mt-5 m-auto" style={{width: '28rem'}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Username: {profile.username}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">Email: {profile.email}</h6>
+                        <h6 className="card-text">Total photos published: {profile.photos}</h6>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
